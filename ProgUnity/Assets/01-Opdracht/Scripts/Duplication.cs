4e0d56e0232c4fbe425f7baa3377f20d54c9e70b
JoyceMolenaar0 <146432917+JoyceMolenaar0@users.noreply.github.com>
2025-01-31 10:19:20 +0100

    BetterScript
    het script is geupdate met een paar returns

--- ProgUnity/Assets/01-Opdracht/Scripts/Duplication.cs
+++ ProgUnity/Assets/01-Opdracht/Scripts/Duplication.cs
@@ -1,70 +1,60 @@
-
-using Unity.VisualScripting;
 using UnityEngine;
-
 
 public class Duplication : MonoBehaviour
 {
     [SerializeField] GameObject BallPrefab;
-
     private float ElapsedTime = 0f;
 
     private void Start()
     {
         for (int i = 0; i < 100; i++)
         {
-            CreateBall();
+            Color color = RandomColor();
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            GameObject ball = CreateBall(color, randPos);
+            DestroyBall(ball);
         }
     }
-
-     void Update()
+    private void Update()
     {
-
-
         ElapsedTime += Time.deltaTime;
         if (ElapsedTime > 1.5f)
         {
-            CreateBall();
-            ElapsedTime = 0f;
+            Color color = RandomColor();
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            GameObject ball = CreateBall(color, randPos);
+            DestroyBall(ball);
 
-        } 
-        
+            ElapsedTime = 0f;
+        }
+    }
+    private GameObject CreateBall(Color c, Vector3 position)
+    {
+        GameObject ball = Instantiate(BallPrefab, position, Quaternion.identity);
+        Renderer ballRenderer = ball.GetComponent<Renderer>();
+
+        ballRenderer.material.color = c;
+
+        return ball; 
     }
 
-    void CreateBall()
+    private void DestroyBall(GameObject ball)
     {
-
-        GameObject Ball = Instantiate(BallPrefab);
-        Material material = Ball.GetComponent<MeshRenderer>().material;
-        Renderer ballRenderer = Ball.GetComponent<Renderer>();
-
-       
-
-        float x = Random.Range(-5f, 5f);
-        float y = Random.Range(-5f, 5f);
-        float z = Random.Range(-5f, 5f);
-
+        Destroy(ball, 2f);
+    }
+    private Color RandomColor()
+    {
         float r = Random.Range(0f, 1f);
         float g = Random.Range(0f, 1f);
         float b = Random.Range(0f, 1f);
-
-
-
-        Vector3 RandomPos = new Vector3 (x, y, z);
-        Color RandomCol = new Color (r, g, b, 1f);
-
-
-        
-
-        Ball.transform.position = RandomPos;
-        ballRenderer.material.color = RandomCol;
-
-        DestroyBall(Ball);
-
+        return new Color(r, g, b, 1f);
     }
 
-    void DestroyBall(GameObject Ball)
+    private Vector3 RandomPosition(float min, float max)
     {
-        Destroy(Ball, 2f);
+        float x = Random.Range(min, max);
+        float y = Random.Range(min, max);
+        float z = Random.Range(min, max);
+        return new Vector3(x, y, z);
     }
 }
